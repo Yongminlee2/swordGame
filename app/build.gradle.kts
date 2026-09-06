34bb99a91ef423a0ca422759d3f7892471ba3db8
Yongminlee2 <dydals5678@gmail.com>
2026-09-06 10:45:56 +0900

    버전을 1.0.2 (versionCode 3) 로 올림
    콘솔에 versionCode 2 까지 올라가 있어 그다음 번호가 필요하다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.geomgang.game"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
